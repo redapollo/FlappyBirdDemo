2c84658b999d52426d61b81eed0d0ef2a046b3f7
RedApollo <redapollo44@gmail.com>
2018-02-25 12:40:49 -0500

    Initial Commit

---
+++ desktop/src/com/redapollo/flappy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.redapollo.flappy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.redapollo.flappy.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
